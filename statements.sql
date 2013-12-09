4ebb344cf99bbc790f56081c46c724cb0ae69eab
deegeee <dg@dayrep.com>
2013-12-09 13:15:59 +0100

    Update statements.sql

--- statements.sql
+++ statements.sql
@@ -1,90 +1,97 @@
-/* Tabellen */
 create table Auto
-(	autoNr integer primary key,
-	farbe varchar2(10),
-	model varchar2(100),
-	ps integer
+(        autoNr integer primary key,
+        farbe varchar(10),
+        model varchar(100),
+        ps integer
 );
+
 
 create table Kunde
 (
-	kundenNr integer primary key,
-	geburtsdatum date,
-	vorname varchar2(50),
-	nachname varchar2(50)
-);	
+        kundenNr integer primary key,
+        vorname varchar(50),
+        nachname varchar(50),
+		geburtsdatum date
+);        
+
 
 create table Mitarbeiter
 (
-	personalNr integer primary key,
-	raum integer,
-	vorname varchar2(50),
-	nachname varchar2(50)
+        personalNr integer primary key,
+        raum integer,
+        vorname varchar(50),
+        nachname varchar(50)
 );
+
 
 create table mietet
 (
-	auftragsNr integer primary key,
-	datum date,
-	kundenNr integer,
-	autoNr integer,
-	personalNr integer,
-	foreign key (kundenNr) references Kunde (kundenNr),
-	foreign key (autoNr) references Auto (autoNr),
-	foreign key (personalNr) references Verkaeufer (personalNr)
+        auftragsNr integer primary key,
+        kundenNr integer,
+		datum date,
+        autoNr integer,
+        personalNr integer,
 );
+
 
 /* Daten */
 
+
 /* PLATZHALTER aus lab1 muss geaendert werden */
+
 
 /* Auto (int, varchar, varchar, int) */
 insert into Auto
-values (1, 'Peter', 'Pan', '77652 Offenburg');
+values (1, 'rot', 'Mercedes SLK 3000', 362);
 insert into Auto
-values (2, 'Klaus', 'Klaus', '77652 Offenburg');
+values (2, 'gelb', 'Porsche 911', 621);
 insert into Auto
-values (3, 'Tom', 'Tom', '77652 Offenburg');
+values (3, 'weiss', 'Seat Ibiza', 105);
 insert into Auto
-values (4, 'Wald', 'Traud', '77652 Offenburg');
+values (4, 'pink', 'Smart', 1001);
 insert into Auto
-values (5, 'Ger', 'Linde', '77652 Offenburg');
+values (5, 'schwarz', 'Hummer', 440);
 
 
-/* Kunde (int, date, varchar, varchar) */
+
+
+/* Kunde (int, varchar, varchar, date) */
 insert into Kunde
-values (1, 'Peter', 'Pan', '77652 Offenburg');
+values (1, 'Peter', 'Pan', '01-JAN-1990 00:00');
 insert into Kunde
-values (2, 'Klaus', 'Klaus', '77652 Offenburg');
+values (2, 'Klaus', 'Klawus', '21-JAN-1980 00:00');
 insert into Kunde
-values (3, 'Tom', 'Tom', '77652 Offenburg');
+values (3, 'Tom', 'Tomer', '01-JAN-1992 00:00');
 insert into Kunde
-values (4, 'Wald', 'Traud', '77652 Offenburg');
+values (4, 'Wald', 'Traud', '02-JAN-1998 00:00');
 insert into Kunde
-values (5, 'Ger', 'Linde', '77652 Offenburg');
+values (5, 'Ger', 'Linde', '01-JAN-1991 00:00');
+
 
 commit;
 
-/* Verkaeufer (int, int, varchar, varchar) */
-insert into Verkaeufer
-values (1, 'James', 'Bond', 1000, 1);
-insert into Verkaeufer
-values (2, 'Jack', 'Bauer', 2000, 2);
-insert into Verkaeufer
-values (3, 'Rowan', 'Atkinson', 2000, 1);
-insert into Verkaeufer
-values (4, 'Ricki', 'Gervais', 2000, 2);
-insert into Verkaeufer
-values (5, 'Milli', 'Vanilli', 2000, 1);
+
+/* Mitarbeiter (int, int, varchar, varchar) */
+insert into Mitarbeiter
+values (1, 7, 'James', 'Bond');
+insert into Mitarbeiter
+values (2, 8, 'Johann', 'Zum');
+insert into Mitarbeiter
+values (3, 9, 'Olewa', 'Kack');
+insert into Mitarbeiter
+values (4, 10, 'Friedrich', 'Shei');
+insert into Mitarbeiter
+values (5, 11, 'Ku Wi', 'Wong');
 
 commit;
 
-/* Auftrag (int, date, int, int, int) */
 
-insert into Auftrag
-values (1, 1, 1, 'Kredit', 1);  
+/* Auftrag (int, int, date, int, int) */
 
-insert into Auftrag
-values (2, 3, 2, 'Leihe', 2);   
 
+insert into mietet
+values (1, 2, '01-JAN-2012 00:00', 3, 2);   
+
+insert into mietet
+values (2, 3, '01-DEZ-2013 00:00', 4, 1);    
 commit;
